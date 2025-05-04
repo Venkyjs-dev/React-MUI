2ae58695e4f8c7b73a4b4bc2e85b087409e3ebd4
Venkatesh Thimmaiah <venkatesh131755@gmail.com>
2025-05-04 13:29:05 +0530

    concept: Typography types

--- src/components/MuiTypography.tsx
+++ src/components/MuiTypography.tsx
@@ -6,9 +6,27 @@ const MuiTypography = () => {
       <Typography variant="h1"> H1 Heading</Typography>
       <Typography variant="h2"> H2 Heading</Typography>
       <Typography variant="h3"> H3 Heading</Typography>
-      <Typography variant="h4"> H4 Heading</Typography>
+      <Typography variant="h4" component="h1" gutterBottom>
+        H4 Heading
+      </Typography>
       <Typography variant="h5"> H5 Heading</Typography>
       <Typography variant="h6"> H6 Heading</Typography>
+
+      <Typography variant="subtitle1"> Subtitle 1</Typography>
+      <Typography variant="subtitle2"> Subtitle 2</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, iusto
+        enim, quia dolore a repellat omnis magnam dolores provident laborum aut,
+        aliquam corrupti repellendus sed? Saepe dolorem non illo odio.
+      </Typography>
+      <hr />
+
+      <Typography variant="body2">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est sunt
+        fugiat ex molestiae libero perferendis voluptatum omnis animi unde.
+        Optio eius eaque iste saepe placeat id nisi quos fuga aliquam.
+      </Typography>
     </section>
   );
 };
